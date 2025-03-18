3d120f011cec3a7f1b46dec4477e4de04dcc691f
geraldofori <oforige@gmail.com>
2025-03-18 00:20:27 +0000

    created a custom layout and navbar components

--- src/router/index.js
+++ src/router/index.js
@@ -4,32 +4,39 @@ import MealList from "../components/views/MealsByName.vue";
 import MealsByName from "../components/views/MealsByName.vue";
 import MealsByLetter from "../components/views/MealsByLetter.vue";
 import MealsByIngredient from "../components/views/MealsByIngredient.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home,
-  },
-  {
-    path: "/letter/:letter",
-    name: "byMealLetter",
-    component: MealList,
-  },
-  {
-    path: "/by-name/:name?",
-    name: "byName",
-    component: MealsByName,
-  },
-  {
-    path: "/by-ingredient/:ingredient?",
-    name: "byIngredient",
-    component: MealsByIngredient,
-  },
-  {
-    path: "/by-letter/:letter?",
-    name: "byLetter",
-    component: MealsByLetter,
+    component: DefaultLayout,
+    children: [
+      {
+        path: "/",
+        name: "home",
+        component: Home,
+      },
+      {
+        path: "/letter/:letter",
+        name: "byMealLetter",
+        component: MealList,
+      },
+      {
+        path: "/by-name/:name?",
+        name: "byName",
+        component: MealsByName,
+      },
+      {
+        path: "/by-ingredient/:ingredient?",
+        name: "byIngredient",
+        component: MealsByIngredient,
+      },
+      {
+        path: "/by-letter/:letter?",
+        name: "byLetter",
+        component: MealsByLetter,
+      },
+    ],
   },
 ];
 export default createRouter({
