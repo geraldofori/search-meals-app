817a8a266105e3d65114e51ab06228ac3f90b3da
geraldofori <oforige@gmail.com>
2025-03-18 01:04:59 +0000

    setup the state for searchedMeals

--- src/components/views/MealsByName.vue
+++ src/components/views/MealsByName.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
-    <p>By Name</p>
+  <div class="p-8">
+    <input
+      v-model="keyword"
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for meals"
+      @change="searcForMeals"
+    />
   </div>
+  <pre>{{ meals }}</pre>
 </template>
 
-<script  setup>
+<script setup>
+import { computed, ref } from "vue";
+import store from "../../store";
+const meals = computed(() => store.state.searchedMeals);
 
+const keyword = ref("");
+function searcForMeals() {
+  console.log("keyword.value", keyword.value);
+  store.dispatch("searchMeals", keyword.value);
+}
 </script>
